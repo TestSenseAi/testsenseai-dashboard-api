ea3eae7b0823c2b2e9168afddb989ebb124dc1fb
Cristian Sarmiento <cristian.sarmiento@carejourney.com>
2025-02-01 11:33:38 -0300

    fix lint

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import typescript from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                project: './tsconfig.json',
+            },
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+        },
+        plugins: {
+            '@typescript-eslint': typescript,
+            prettier: prettier,
+        },
+        rules: {
+            ...typescript.configs.recommended.rules,
+            'prettier/prettier': 'error',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                },
+            ],
+        },
+    },
+    {
+        ignores: ['node_modules/**', 'dist/**', 'build/**', 'coverage/**', '.nitric/**'],
+    },
+];
